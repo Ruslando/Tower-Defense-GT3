b83198f71c16a5cb93befe98d2c0b356633f6ccb
Ruslan Novikov <ruslan.novikov@hotmail.de>
2024-02-09 21:25:58 +0100

    fix: fix mistake where laps would not show up correctly

--- Assets/Scripts/Spawner/KartManager.cs
+++ Assets/Scripts/Spawner/KartManager.cs
@@ -51,6 +51,7 @@ public class KartManager : Singleton<KartManager>
 
     private void SetupKarts()
     {
+        CurrentLap = 0;
         ResetAndRemoveAllKarts();
         PlaceAndSpawnKarts();
     }
